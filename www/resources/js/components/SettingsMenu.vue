43715929f2d923eaeeae270a85e35c594608cf32
Hubblika <hubblika@gmail.com>
2025-12-18 16:53:07 +0100

    Tweaked design of the settings page

--- www/resources/js/components/SettingsMenu.vue
+++ www/resources/js/components/SettingsMenu.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ToggleSwitch } from './common';
+import { ToggleSwitch, Icon } from './common';
 import { ref, watch, onMounted } from 'vue';
 
 const isDark = ref(false);
@@ -11,20 +11,46 @@ watch(isDark, (value) => {
 })
 
 onMounted(() => {
-  isDark.value = localStorage.getItem('theme') === 'dark'
+    isDark.value = localStorage.getItem('theme') === 'dark'
 })
 </script>
 
 <template>
-    <div class="flex flex-col color-black dark:color-white gap-4">
-        <h1 class="font-bold">General</h1>
-        <div class="flex justify-between">
-            <p class="color-black dark:color-white">Theme - change between light and dark themes</p>
-            <ToggleSwitch v-model="isDark"></ToggleSwitch>
+    <div class="flex flex-col gap-4">
+        <h1 class="text-2xl font-bold text-gray-900 dark:text-white">General Settings</h1>
+
+        <div
+            class="flex justify-between items-center w-full p-4 bg-gray-100 dark:bg-black rounded-xl shadow-inner transition-colors duration-300 hover:bg-gray-200 dark:hover:bg-gray-700">
+
+            <p class="text-gray-800 dark:text-gray-200 font-medium">
+                Theme - switch between light and dark mode
+            </p>
+
+            <div class="flex items-center gap-3">
+                <p class="text-gray-800 dark:text-gray-200 font-semibold">
+                    {{ isDark ? 'Dark' : 'Light' }}
+                </p>
+
+                <Icon :name="isDark ? 'moon' : 'sun'" class="w-5 h-5 text-yellow-500 dark:text-white"/>
+
+                <ToggleSwitch v-model="isDark"/>
+            </div>
         </div>
-        <div class="flex justify-between">
-            <p class="color-black dark:color-white">Language - change the language of the application</p>
-            <ToggleSwitch v-model="isEnglish"></ToggleSwitch>
+
+        <div
+            class="flex justify-between items-center w-full p-4 bg-gray-100 dark:bg-black rounded-xl shadow-inner transition-colors duration-300 hover:bg-gray-200 dark:hover:bg-gray-700">
+
+            <p class="text-gray-800 dark:text-gray-200 font-medium">
+                Theme - switch app language (EN/HUN)
+            </p>
+
+            <div class="flex items-center gap-3">
+                <p class="text-gray-800 dark:text-gray-200 font-semibold">
+                    {{ isEnglish ? 'English' : 'Magyar' }}
+                </p>
+
+                <ToggleSwitch v-model="isEnglish"/>
+            </div>
         </div>
     </div>
 </template>
